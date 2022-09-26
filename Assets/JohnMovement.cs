5c97eed6f9c940373870db01a9e8e345baef9fa1
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2022-09-26 12:36:02 +0200

    add Grounded control

--- Assets/JohnMovement.cs
+++ Assets/JohnMovement.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JohnMovement : MonoBehaviour
 {
+    public float JumpForce;
+    public float Speed;
+    public bool Grounded;
+
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
 
@@ -11,16 +16,31 @@ public class JohnMovement : MonoBehaviour
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        JumpForce = 120;
+        Speed = 1.1f;
+        Grounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");        
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        // Check if is grounded        
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f)) Grounded = true;
+        else Grounded = false;
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded) Jump();
+                 
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce); 
     }
 }
