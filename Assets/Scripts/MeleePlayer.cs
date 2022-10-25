62517cf89e914017e202e3207de0bdfd78f9fcf1
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2022-10-25 23:18:47 +0200

    create a new tile palette for melee combat scene

--- Assets/Scripts/MeleePlayer.cs
+++ Assets/Scripts/MeleePlayer.cs
@@ -4,9 +4,12 @@ public class MeleePlayer : MonoBehaviour
 {
 
     public float speed;
+    public float jumpForce;
 
     private float horizontal;
     private bool running;
+    public bool grounded;
+    public float distanceToGround;
 
     private Animator animatorComp;
     private Rigidbody2D rigidbody2DComp;
@@ -15,8 +18,13 @@ public class MeleePlayer : MonoBehaviour
     void Start()
     {
         speed = 5.5f;
+        jumpForce = 700f;
         animatorComp = GetComponent<Animator>();
         rigidbody2DComp = GetComponent<Rigidbody2D>();
+        rigidbody2DComp.mass = 1.4f;
+        rigidbody2DComp.gravityScale = 3.3f;
+        grounded = true;
+        distanceToGround = 1f;
 
     }
 
@@ -33,10 +41,19 @@ public class MeleePlayer : MonoBehaviour
         // Turn sprite according movement orientation
         if (horizontal < 0.0f) transform.localScale = new Vector3(-1, 1, 1);
         else if (horizontal > 0.0f) transform.localScale = new Vector3(1, 1, 1);
+
+        // Jump
+        grounded = Physics2D.Raycast(transform.position, Vector3.down, distanceToGround) ? true : false;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) Jump();
     }
 
     private void FixedUpdate()
     {
         rigidbody2DComp.velocity = new Vector2(horizontal * speed, rigidbody2DComp.velocity.y);
     }
+
+    private void Jump() {        
+        rigidbody2DComp.AddForce(Vector2.up * jumpForce);
+    }
+
 }
