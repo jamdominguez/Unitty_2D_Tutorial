0f84195a9fa4fb806d45c3cb3f456c9befaf3b68
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2022-11-06 20:15:03 +0100

    manage scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour    
 {
@@ -18,6 +19,7 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+        SceneManager.LoadScene("SceneName");
     }
 
     public void AddEnemiesDefeat(int newEnemiesDefeat) {
